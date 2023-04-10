15b22b52d8d8dda16d9c81b044f33657015460e7
fagner de oliveira carrena <fagnercarrena@yahoo.com.br>
2023-04-10 09:31:52 -0300

    first commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,15 @@
+import './Banner.css'
+
+
+
+function Banner(){
+return(
+    <header className='banner'>
+        <img src="/imagens/imagens/banner.png" alt="O primeiro componente do projeto"/>
+</header>
+    
+)
+
+}
+
+export default Banner;
