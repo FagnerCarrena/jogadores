fe37ce683ed898c939cf32e41d7a807c5a4aff96
fagner de oliveira carrena <fagnercarrena@yahoo.com.br>
2023-04-13 08:44:35 -0300

    adicionando

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -2,8 +2,10 @@ import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
+import {useState} from "react"
 
-const Formulario = ()=>{
+
+const Formulario = (props)=>{
 const times = [
 'Bahia',
 'Vitória',
@@ -14,19 +16,54 @@ const times = [
 'Botafogo'
 
 ]
+const [nome, setNome] = useState('')
+const [funcao, setFuncao] = useState('')
+const [imagem, setImagem] = useState('')
+const [time, setTime] = useState('')
+
+
+
 const aoSalvar = (evento)=>{
-  evento.preventDefault()
-  console.log("for foi submetido")
+  evento.preventDefault();
+  props.aoColaborodorCadastrado({
+    nome,
+    funcao,
+    imagem,
+    time
+  })
 }
 
 return (
 <section className="formulario">
     <form onSubmit={aoSalvar} >
         <h2 >Prencha os Dados para criar o card do Jogador</h2>
-    <CampoTexto obrigatorio={true}  label="Nome" placeholder="Digite seu nome" />
-      <CampoTexto  obrigatorio={true} label="Função" placeholder="Digite sua Função"/>
-      <CampoTexto label="Imagem" placeholder="Digite o endereço da Imagem"/>
-      <ListaSuspensa obrigatorio={true} label="Time" itens = {times}/>
+    <CampoTexto 
+    obrigatorio={true} 
+     label="Nome" 
+     placeholder="Digite seu nome"
+    valor={nome}
+    aoAlterado={fff=>setNome(fff)} 
+     />
+      <CampoTexto  
+      obrigatorio={true} 
+      label="Função" 
+      placeholder="Digite sua Funcao"
+      valor={funcao}
+    aoAlterado={ggg=>setFuncao(ggg)} 
+      />
+      <CampoTexto
+       label="Imagem" 
+       placeholder="Digite o endereço da Imagem"
+       valor={imagem}
+    aoAlterado={lalala=>setImagem(lalala)} 
+       />
+      <ListaSuspensa
+       obrigatorio={true}
+       label="Time" 
+       itens = {times}
+       valor={time}
+       aoAlterado={valor=>setTime(valor)}
+       />
       <Botao>
       Criar Card
       </Botao>
